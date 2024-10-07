696dd2a7e7e73158c4f845aff64a5c909ccd26f9
Ravi Mistry <lh23197@bristol.ac.uk>
2024-10-07 20:36:44 +0100

    make consistent the match statements for parse functions

--- src/recogniser.rs
+++ src/recogniser.rs
@@ -30,11 +30,21 @@ impl Recogniser {
     fn parse_prog(&mut self) {
         let c = self.peek().unwrap();
         match c {
-            Token::If => { self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar)},
-            Token::While => { self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar)},
-            Token::Skip => { self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar)},
-            Token::Id(_) => { self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar)},
-            Token::LeftCurly => { self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar)},
+            Token::If => {
+                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
+            },
+            Token::While => {
+                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
+            },
+            Token::Skip => {
+                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
+            },
+            Token::Id(_) => {
+                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
+            },
+            Token::LeftCurly => {
+                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
+            },
             _ => panic!(),
         };
     }
@@ -42,11 +52,21 @@ impl Recogniser {
     fn parse_stmt(&mut self)  {
         let c = self.peek().unwrap();
         match c {
-            Token::If => {self.eat(Token::If); self.parse_bexp(); self.eat(Token::Then); self.parse_stmt(); self.eat(Token::Else); self.parse_stmt()},
-            Token::While => {self.eat(Token::While); self.parse_bexp(); self.eat(Token::Do); self.parse_stmt()},
-            Token::Skip => {self.eat(Token::Skip)},
-            Token::Id(s) => {self.eat(Token::Id(s)); self.eat(Token::Assignment); self.parse_aexp()},
-            Token::LeftCurly => {self.eat(Token::LeftCurly); self.parse_stmt();self.parse_stmts(); self.eat(Token::RightCurly)},
+            Token::If => {
+                self.eat(Token::If); self.parse_bexp(); self.eat(Token::Then); self.parse_stmt(); self.eat(Token::Else); self.parse_stmt();
+            },
+            Token::While => {
+                self.eat(Token::While); self.parse_bexp(); self.eat(Token::Do); self.parse_stmt();
+            },
+            Token::Skip => {
+                self.eat(Token::Skip);
+            },
+            Token::Id(s) => {
+                self.eat(Token::Id(s)); self.eat(Token::Assignment); self.parse_aexp();
+            },
+            Token::LeftCurly => {
+                self.eat(Token::LeftCurly); self.parse_stmt();self.parse_stmts(); self.eat(Token::RightCurly);
+            },
             _ => panic!(),
 
         };
@@ -58,7 +78,9 @@ impl Recogniser {
         match c {
             Token::Dollar => {},
             Token::RightCurly => {},
-            Token::Semicolon => {self.eat(Token::Semicolon); self.parse_stmt(); self.parse_stmts();},
+            Token::Semicolon => {
+                self.eat(Token::Semicolon); self.parse_stmt(); self.parse_stmts();
+            },
             _ => panic!(),
 
         };
@@ -68,12 +90,24 @@ impl Recogniser {
     fn parse_bexp(&mut self) {
         let c = self.peek().unwrap();
         match c {
-            Token::Id(_) => {self.parse_bfac(); self.parse_bexps()},
-            Token::Not => {self.parse_bfac(); self.parse_bexps()},
-            Token::Num(_) => {self.parse_bfac(); self.parse_bexps()},
-            Token::True => {self.parse_bfac(); self.parse_bexps()},
-            Token::False => {self.parse_bfac(); self.parse_bexps()},
-            Token::LeftParenthesis => {self.parse_bfac(); self.parse_bexps()},
+            Token::Id(_) => {
+                self.parse_bfac(); self.parse_bexps();
+            },
+            Token::Not => {
+                self.parse_bfac(); self.parse_bexps();
+            },
+            Token::Num(_) => {
+                self.parse_bfac(); self.parse_bexps();
+            },
+            Token::True => {
+                self.parse_bfac(); self.parse_bexps();
+            },
+            Token::False => {
+                self.parse_bfac(); self.parse_bexps();
+            },
+            Token::LeftParenthesis => {
+                self.parse_bfac(); self.parse_bexps();
+            },
             _ => panic!(),
 
         };
@@ -84,7 +118,9 @@ impl Recogniser {
         match c {
             Token::Then => {},
             Token::Do => {},
-            Token::Or => {self.eat(Token::Or); self.parse_bfac(); self.parse_bexps()},
+            Token::Or => {
+                self.eat(Token::Or); self.parse_bfac(); self.parse_bexps();
+            },
             Token::RightParenthesis => {},
             _ => panic!(),
 
@@ -95,12 +131,24 @@ impl Recogniser {
     fn parse_bfac(&mut self) {
         let c = self.peek().unwrap();
         match c {
-            Token::Id(_) => {self.parse_bneg(); self.parse_bfacs()},
-            Token::Not => {self.parse_bneg(); self.parse_bfacs()},
-            Token::Num(_) => {self.parse_bneg();self.parse_bfacs()},
-            Token::True => {self.parse_bneg(); self.parse_bfacs()},
-            Token::False => {self.parse_bneg(); self.parse_bfacs()},
-            Token::LeftParenthesis => {self.parse_bneg(); self.parse_bfacs()},
+            Token::Id(_) => {
+                self.parse_bneg(); self.parse_bfacs();
+            },
+            Token::Not => {
+                self.parse_bneg(); self.parse_bfacs();
+            },
+            Token::Num(_) => {
+                self.parse_bneg();self.parse_bfacs();
+            },
+            Token::True => {
+                self.parse_bneg(); self.parse_bfacs();
+            },
+            Token::False => {
+                self.parse_bneg(); self.parse_bfacs();
+            },
+            Token::LeftParenthesis => {
+                self.parse_bneg(); self.parse_bfacs();
+            },
             _ => panic!(),
 
         };
@@ -112,7 +160,9 @@ impl Recogniser {
             Token::Then => {},
             Token::Do => {},
             Token::Or => {},
-            Token::And => {self.eat(Token::And); self.parse_bneg(); self.parse_bfacs()},
+            Token::And => {
+                self.eat(Token::And); self.parse_bneg(); self.parse_bfacs();
+            },
             Token::RightParenthesis => {},
             _ => panic!(),
 
@@ -122,12 +172,24 @@ impl Recogniser {
     fn parse_bneg(&mut self) {
         let c = self.peek().unwrap();
         match c {
-            Token::Id(_) => {self.parse_brel()},
-            Token::Not => {self.eat(Token::Not); self.parse_bneg()},
-            Token::Num(_) => {self.parse_brel()},
-            Token::True => {self.parse_brel()},
-            Token::False => {self.parse_brel()},
-            Token::LeftParenthesis => {self.parse_brel()},
+            Token::Id(_) => {
+                self.parse_brel();
+            },
+            Token::Not => {
+                self.eat(Token::Not); self.parse_bneg();
+            },
+            Token::Num(_) => {
+                self.parse_brel();
+            },
+            Token::True => {
+                self.parse_brel();
+            },
+            Token::False => {
+                self.parse_brel();
+            },
+            Token::LeftParenthesis => {
+                self.parse_brel();
+            },
             _ => panic!(),
 
         };
@@ -137,11 +199,21 @@ impl Recogniser {
     fn parse_brel(&mut self) {
         let c = self.peek().unwrap();
         match c {
-            Token::Id(_) => {self.parse_aexp(); self.parse_brels()},
-            Token::Num(_) => {self.parse_aexp(); self.parse_brels()},
-            Token::True => {self.parse_aexp(); self.parse_brels()},
-            Token::False => {self.parse_aexp(); self.parse_brels()},
-            Token::LeftParenthesis => {self.parse_aexp(); self.parse_brels()},
+            Token::Id(_) => {
+                self.parse_aexp(); self.parse_brels();
+            },
+            Token::Num(_) => {
+                self.parse_aexp(); self.parse_brels();
+            },
+            Token::True => {
+                self.parse_aexp(); self.parse_brels();
+            },
+            Token::False => {
+                self.parse_aexp(); self.parse_brels();
+            },
+            Token::LeftParenthesis => {
+                self.parse_aexp(); self.parse_brels();
+            },
             _ => panic!(),
 
         };
@@ -155,8 +227,12 @@ impl Recogniser {
             Token::Do => {},
             Token::Or => {},
             Token::And => {},
-            Token::LessThan => {self.eat(Token::LessThan); self.parse_aexp()},
-            Token::Equals => {self.eat(Token::Equals); self.parse_aexp()},
+            Token::LessThan => {
+                self.eat(Token::LessThan); self.parse_aexp();
+            },
+            Token::Equals => {
+                self.eat(Token::Equals); self.parse_aexp();
+            },
             Token::RightParenthesis => {},
             _ => panic!(),
 
@@ -167,11 +243,21 @@ impl Recogniser {
     fn parse_aexp(&mut self) {
         let c = self.peek().unwrap();
         match c {
-            Token::Id(_) => {self.parse_afac(); self.parse_aexps()},
-            Token::Num(_) => {self.parse_afac(); self.parse_aexps()},
-            Token::True => {self.parse_afac(); self.parse_aexps()},
-            Token::False => {self.parse_afac(); self.parse_aexps()},
-            Token::LeftParenthesis => {self.parse_afac(); self.parse_aexps()},
+            Token::Id(_) => {
+                self.parse_afac(); self.parse_aexps();
+            },
+            Token::Num(_) => {
+                self.parse_afac(); self.parse_aexps();
+            },
+            Token::True => {
+                self.parse_afac(); self.parse_aexps();
+            },
+            Token::False => {
+                self.parse_afac(); self.parse_aexps();
+            },
+            Token::LeftParenthesis => {
+                self.parse_afac(); self.parse_aexps();
+            },
             _ => panic!(),
 
         };
@@ -191,8 +277,12 @@ impl Recogniser {
             Token::And => {},
             Token::LessThan => {},
             Token::Equals => {},
-            Token::Plus => {self.eat(Token::Plus);self.parse_afac(); self.parse_aexps()},
-            Token::Minus => {self.eat(Token::Minus); self.parse_afac(); self.parse_aexps()},
+            Token::Plus => {
+                self.eat(Token::Plus);self.parse_afac(); self.parse_aexps();
+            },
+            Token::Minus => {
+                self.eat(Token::Minus); self.parse_afac(); self.parse_aexps();
+            },
             Token::RightParenthesis => {},
             _ => panic!(),
 
@@ -202,11 +292,21 @@ impl Recogniser {
     fn parse_afac(&mut self) {
         let c = self.peek().unwrap();
         match c {
-            Token::Id(_) => {self.parse_atom(); self.parse_afacs()},
-            Token::Num(_) => {self.parse_atom(); self.parse_afacs()},
-            Token::True => {self.parse_atom(); self.parse_afacs()},
-            Token::False => {self.parse_atom(); self.parse_afacs()},
-            Token::LeftParenthesis => {self.parse_atom(); self.parse_afacs()},
+            Token::Id(_) => {
+                self.parse_atom(); self.parse_afacs();
+            },
+            Token::Num(_) => {
+                self.parse_atom(); self.parse_afacs();
+            },
+            Token::True => {
+                self.parse_atom(); self.parse_afacs();
+            },
+            Token::False => {
+                self.parse_atom(); self.parse_afacs();
+            },
+            Token::LeftParenthesis => {
+                self.parse_atom(); self.parse_afacs();
+            },
             _ => panic!(),
 
         };
@@ -227,7 +327,9 @@ impl Recogniser {
             Token::Equals =>{},
             Token::Plus => {},
             Token::Minus => {},
-            Token::Asterisk => {self.eat(Token::Asterisk); self.parse_atom(); self.parse_afacs()},
+            Token::Asterisk => {
+                self.eat(Token::Asterisk); self.parse_atom(); self.parse_afacs();
+            },
             Token::RightParenthesis => {},
             _ => panic!(),
 
@@ -236,11 +338,21 @@ impl Recogniser {
     fn parse_atom(&mut self) {
         let c = self.peek().unwrap();
         match c {
-            Token::Id(s) => {self.eat(Token::Id(s))},
-            Token::Num(s) => {self.eat(Token::Num(s))},
-            Token::True => {self.eat(Token::True)},
-            Token::False => {self.eat(Token::False)},
-            Token::LeftParenthesis => {self.eat(Token::LeftParenthesis); self.parse_bexp(); self.eat(Token::RightParenthesis)},
+            Token::Id(s) => {
+                self.eat(Token::Id(s));
+            },
+            Token::Num(s) => {
+                self.eat(Token::Num(s));
+            },
+            Token::True => {
+                self.eat(Token::True);
+            },
+            Token::False => {
+                self.eat(Token::False);
+            },
+            Token::LeftParenthesis => {
+                self.eat(Token::LeftParenthesis); self.parse_bexp(); self.eat(Token::RightParenthesis);
+            },
             _ => panic!(),
 
         };
