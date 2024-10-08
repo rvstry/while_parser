cb36428295c43e63e476e2496469a650f280382a
Ravi Mistry <lh23197@bristol.ac.uk>
2024-10-08 01:13:52 +0100

    implement parser

---
+++ src/ast.rs
@@ -0,0 +1,29 @@
+pub trait Ast {
+}
+
+pub enum Exp {
+    True,
+    False,
+    Less(Box<Exp>, Box<Exp>),
+    Eq(Box<Exp>, Box<Exp>),
+    Not(Box<Exp>),
+    And(Box<Exp>, Box<Exp>),
+    Or(Box<Exp>, Box<Exp>),
+    Var(String),
+    Num(i32),
+    Plus(Box<Exp>, Box<Exp>),
+    Minus(Box<Exp>, Box<Exp>),
+    Times(Box<Exp>, Box<Exp>),
+}
+
+impl Ast for Exp {}
+
+pub enum Stmt {
+    Skip,
+    Assn(String, Box<Exp>),
+    Seq(Box<Stmt>, Box<Stmt>),
+    Cond(Box<Exp>, Box<Stmt>, Box<Stmt>),
+    While(Box<Exp>, Box<Stmt>),
+}
+
+impl Ast for Stmt {}
--- src/recogniser.rs
+++ src/recogniser.rs
@@ -1,8 +1,13 @@
 use std::collections::VecDeque;
 use crate::token::Token;
+use crate::ast::*;
 
 struct Recogniser {
     input: VecDeque<Token>,
+}
+
+enum WhileError {
+    ParseError,
 }
 
 impl Recogniser {
@@ -27,335 +32,386 @@ impl Recogniser {
 
     }
 
-    fn parse_prog(&mut self) {
+    fn parse_prog(&mut self) -> Result<Stmt, WhileError> {
         let c = self.peek().unwrap();
         match c {
             Token::If => {
-                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
+                let st1 = self.parse_stmt()?; let st2 = self.parse_stmts(st1); self.eat(Token::Dollar);
+                st2
             },
             Token::While => {
-                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
+                let st1 = self.parse_stmt()?; let st2 = self.parse_stmts(st1); self.eat(Token::Dollar);
+                st2
             },
             Token::Skip => {
-                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
-            },
+                let st1 = self.parse_stmt()?; let st2 = self.parse_stmts(st1); self.eat(Token::Dollar);
+                st2
+           },
             Token::Id(_) => {
-                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
-            },
+                let st1 = self.parse_stmt()?; let st2 = self.parse_stmts(st1); self.eat(Token::Dollar);
+                st2
+         },
             Token::LeftCurly => {
-                self.parse_stmt(); self.parse_stmts(); self.eat(Token::Dollar);
-            },
-            _ => panic!(),
-        };
+                let st1 = self.parse_stmt()?; let st2 = self.parse_stmts(st1); self.eat(Token::Dollar);
+                st2
+             },
+            _ => Err(WhileError::ParseError),
+        }
     }
 
-    fn parse_stmt(&mut self)  {
+    fn parse_stmt(&mut self) -> Result<Stmt, WhileError> {
         let c = self.peek().unwrap();
         match c {
             Token::If => {
-                self.eat(Token::If); self.parse_bexp(); self.eat(Token::Then); self.parse_stmt(); self.eat(Token::Else); self.parse_stmt();
+                self.eat(Token::If); let s1 = self.parse_bexp()?; self.eat(Token::Then); let s2 = self.parse_stmt()?; self.eat(Token::Else); let s3 = self.parse_stmt()?;
+                Ok(Stmt::Cond(Box::new(s1), Box::new(s2), Box::new(s3)))
             },
             Token::While => {
-                self.eat(Token::While); self.parse_bexp(); self.eat(Token::Do); self.parse_stmt();
+                self.eat(Token::While); let s1 = self.parse_bexp()?; self.eat(Token::Do); let s2 = self.parse_stmt()?;
+                Ok(Stmt::While(Box::new(s1), Box::new(s2)))
             },
             Token::Skip => {
                 self.eat(Token::Skip);
+                Ok(Stmt::Skip)
             },
             Token::Id(s) => {
-                self.eat(Token::Id(s)); self.eat(Token::Assignment); self.parse_aexp();
+                self.eat(Token::Id(s.clone())); self.eat(Token::Assignment); let s1 = self.parse_aexp()?;
+                Ok(Stmt::Assn(s, Box::new(s1)))
             },
             Token::LeftCurly => {
-                self.eat(Token::LeftCurly); self.parse_stmt();self.parse_stmts(); self.eat(Token::RightCurly);
+                self.eat(Token::LeftCurly); let s1 = self.parse_stmt()?; let s2 = self.parse_stmts(s1); self.eat(Token::RightCurly);
+                s2
             },
-            _ => panic!(),
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
 
-    fn parse_stmts(&mut self)  {
+    fn parse_stmts(&mut self, sub: Stmt/*impl Ast*/) -> Result<Stmt, WhileError>  {
         let c = self.peek().unwrap();
         match c {
-            Token::Dollar => {},
-            Token::RightCurly => {},
+            Token::Dollar => {/**/Ok(sub)},
+            Token::RightCurly => {/**/Ok(sub)},
             Token::Semicolon => {
-                self.eat(Token::Semicolon); self.parse_stmt(); self.parse_stmts();
+                self.eat(Token::Semicolon); let s1 = self.parse_stmt()?; let s2 = self.parse_stmts(s1)?;
+                Ok(Stmt::Seq(Box::new(sub), Box::new(s2)))
             },
-            _ => panic!(),
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
 
-    fn parse_bexp(&mut self) {
+    fn parse_bexp(&mut self) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
             Token::Id(_) => {
-                self.parse_bfac(); self.parse_bexps();
+                let st1 = self.parse_bfac()?; let st2 = self.parse_bexps(st1);
+                st2
             },
             Token::Not => {
-                self.parse_bfac(); self.parse_bexps();
+                let st1 = self.parse_bfac()?; let st2 = self.parse_bexps(st1);
+                st2
             },
             Token::Num(_) => {
-                self.parse_bfac(); self.parse_bexps();
+                let st1 = self.parse_bfac()?; let st2 = self.parse_bexps(st1);
+                st2
             },
             Token::True => {
-                self.parse_bfac(); self.parse_bexps();
+                let st1 = self.parse_bfac()?; let st2 = self.parse_bexps(st1);
+                st2
             },
             Token::False => {
-                self.parse_bfac(); self.parse_bexps();
+                let st1 = self.parse_bfac()?; let st2 = self.parse_bexps(st1);
+                st2
             },
             Token::LeftParenthesis => {
-                self.parse_bfac(); self.parse_bexps();
+                let st1 = self.parse_bfac()?; let st2 = self.parse_bexps(st1);
+                st2
             },
-            _ => panic!(),
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
-    fn parse_bexps(&mut self) {
+    fn parse_bexps(&mut self, sub: Exp /*impl Ast*/) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
-            Token::Then => {},
-            Token::Do => {},
+            Token::Then => {/**/Ok(sub)},
+            Token::Do => {/**/Ok(sub)},
             Token::Or => {
-                self.eat(Token::Or); self.parse_bfac(); self.parse_bexps();
+                self.eat(Token::Or); let s1 = self.parse_bfac()?; let s2 = self.parse_bexps(s1)?;
+                Ok(Exp::Or(Box::new(sub), Box::new(s2)))
             },
-            Token::RightParenthesis => {},
-            _ => panic!(),
+            Token::RightParenthesis => {/**/Ok(sub)},
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
 
-    fn parse_bfac(&mut self) {
+    fn parse_bfac(&mut self) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
             Token::Id(_) => {
-                self.parse_bneg(); self.parse_bfacs();
+                let st1 = self.parse_bneg()?; let st2 = self.parse_bfacs(st1);
+                st2
             },
             Token::Not => {
-                self.parse_bneg(); self.parse_bfacs();
-            },
+                let st1 = self.parse_bneg()?; let st2 = self.parse_bfacs(st1);
+                st2
+             },
             Token::Num(_) => {
-                self.parse_bneg();self.parse_bfacs();
-            },
+                let st1 = self.parse_bneg()?; let st2 = self.parse_bfacs(st1);
+                st2
+             },
             Token::True => {
-                self.parse_bneg(); self.parse_bfacs();
-            },
+                let st1 = self.parse_bneg()?; let st2 = self.parse_bfacs(st1);
+                st2
+             },
             Token::False => {
-                self.parse_bneg(); self.parse_bfacs();
-            },
+                let st1 = self.parse_bneg()?; let st2 = self.parse_bfacs(st1);
+                st2
+             },
             Token::LeftParenthesis => {
-                self.parse_bneg(); self.parse_bfacs();
-            },
-            _ => panic!(),
+                let st1 = self.parse_bneg()?; let st2 = self.parse_bfacs(st1);
+                st2
+             },
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
-    fn parse_bfacs(&mut self)  {
+    fn parse_bfacs(&mut self, sub: Exp/*impl Ast*/) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
-            Token::Then => {},
-            Token::Do => {},
-            Token::Or => {},
+            Token::Then => {/**/Ok(sub)},
+            Token::Do => {/**/Ok(sub)},
+            Token::Or => {/**/Ok(sub)},
             Token::And => {
-                self.eat(Token::And); self.parse_bneg(); self.parse_bfacs();
+                self.eat(Token::And); let s1 = self.parse_bneg()?; let s2 = self.parse_bfacs(s1)?;
+                Ok(Exp::And(Box::new(sub), Box::new(s2)))
             },
-            Token::RightParenthesis => {},
-            _ => panic!(),
+            Token::RightParenthesis => {/**/Ok(sub)},
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
-    fn parse_bneg(&mut self) {
+    fn parse_bneg(&mut self) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
             Token::Id(_) => {
-                self.parse_brel();
+                self.parse_brel()
             },
             Token::Not => {
-                self.eat(Token::Not); self.parse_bneg();
+                self.eat(Token::Not); let s1 = self.parse_bneg()?;
+                Ok(Exp::Not(Box::new(s1)))
             },
             Token::Num(_) => {
-                self.parse_brel();
+                self.parse_brel()
             },
             Token::True => {
-                self.parse_brel();
+                self.parse_brel()
             },
             Token::False => {
-                self.parse_brel();
+                self.parse_brel()
             },
             Token::LeftParenthesis => {
-                self.parse_brel();
+                self.parse_brel()
             },
-            _ => panic!(),
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
 
-    fn parse_brel(&mut self) {
+    fn parse_brel(&mut self) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
             Token::Id(_) => {
-                self.parse_aexp(); self.parse_brels();
+                let st1 = self.parse_aexp()?; let st2 = self.parse_brels(st1);
+                st2
             },
             Token::Num(_) => {
-                self.parse_aexp(); self.parse_brels();
+                let st1 = self.parse_aexp()?; let st2 = self.parse_brels(st1);
+                st2
             },
             Token::True => {
-                self.parse_aexp(); self.parse_brels();
+                let st1 = self.parse_aexp()?; let st2 = self.parse_brels(st1);
+                st2
             },
             Token::False => {
-                self.parse_aexp(); self.parse_brels();
+                let st1 = self.parse_aexp()?; let st2 = self.parse_brels(st1);
+                st2
             },
             Token::LeftParenthesis => {
-                self.parse_aexp(); self.parse_brels();
+                let st1 = self.parse_aexp()?; let st2 = self.parse_brels(st1);
+                st2
             },
-            _ => panic!(),
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
 
-    fn parse_brels(&mut self) {
+    fn parse_brels(&mut self, sub: Exp/*impl Ast*/) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
-            Token::Then => {},
-            Token::Do => {},
-            Token::Or => {},
-            Token::And => {},
+            Token::Then => {/**/Ok(sub)},
+            Token::Do => {/**/Ok(sub)},
+            Token::Or => {/**/Ok(sub)},
+            Token::And => {/**/Ok(sub)},
             Token::LessThan => {
-                self.eat(Token::LessThan); self.parse_aexp();
+                self.eat(Token::LessThan); let s1 = self.parse_aexp()?;
+                Ok(Exp::Less(Box::new(sub), Box::new(s1)))
             },
             Token::Equals => {
-                self.eat(Token::Equals); self.parse_aexp();
+                self.eat(Token::Equals); let s1 = self.parse_aexp()?;
+                Ok(Exp::Eq(Box::new(sub), Box::new(s1)))
             },
-            Token::RightParenthesis => {},
-            _ => panic!(),
+            Token::RightParenthesis => {/**/Ok(sub)},
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
 
-    fn parse_aexp(&mut self) {
+    fn parse_aexp(&mut self) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
             Token::Id(_) => {
-                self.parse_afac(); self.parse_aexps();
+                let st1 = self.parse_afac()?; let st2 = self.parse_aexps(st1);
+                st2
             },
             Token::Num(_) => {
-                self.parse_afac(); self.parse_aexps();
+                let st1 = self.parse_afac()?; let st2 = self.parse_aexps(st1);
+                st2
             },
             Token::True => {
-                self.parse_afac(); self.parse_aexps();
+                let st1 = self.parse_afac()?; let st2 = self.parse_aexps(st1);
+                st2
             },
             Token::False => {
-                self.parse_afac(); self.parse_aexps();
+                let st1 = self.parse_afac()?; let st2 = self.parse_aexps(st1);
+                st2
             },
             Token::LeftParenthesis => {
-                self.parse_afac(); self.parse_aexps();
+                let st1 = self.parse_afac()?; let st2 = self.parse_aexps(st1);
+                st2
             },
-            _ => panic!(),
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
 
-    fn parse_aexps(&mut self) {
+    fn parse_aexps(&mut self, sub: Exp/*impl Ast*/) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
-            Token::Dollar => {},
-            Token::Then =>{},
-            Token::Else => {},
-            Token::Do => {},
-            Token::RightCurly => {},
-            Token::Semicolon => {},
-            Token::Or => {},
-            Token::And => {},
-            Token::LessThan => {},
-            Token::Equals => {},
+            Token::Dollar => {/**/Ok(sub)},
+            Token::Then =>{/**/Ok(sub)},
+            Token::Else => {/**/Ok(sub)},
+            Token::Do => {/**/Ok(sub)},
+            Token::RightCurly => {/**/Ok(sub)},
+            Token::Semicolon => {/**/Ok(sub)},
+            Token::Or => {/**/Ok(sub)},
+            Token::And => {/**/Ok(sub)},
+            Token::LessThan => {/**/Ok(sub)},
+            Token::Equals => {/**/Ok(sub)},
             Token::Plus => {
-                self.eat(Token::Plus);self.parse_afac(); self.parse_aexps();
+                self.eat(Token::Plus); let s1 = self.parse_afac()?; let s2 = self.parse_aexps(s1)?;
+                Ok(Exp::Plus(Box::new(sub), Box::new(s2)))
             },
             Token::Minus => {
-                self.eat(Token::Minus); self.parse_afac(); self.parse_aexps();
+                self.eat(Token::Minus); let s1 = self.parse_afac()?; let s2 = self.parse_aexps(s1)?;
+                Ok(Exp::Minus(Box::new(sub), Box::new(s2)))
             },
-            Token::RightParenthesis => {},
-            _ => panic!(),
+            Token::RightParenthesis => {/**/Ok(sub)},
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
-    fn parse_afac(&mut self) {
+    fn parse_afac(&mut self) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
             Token::Id(_) => {
-                self.parse_atom(); self.parse_afacs();
+                let st1 = self.parse_atom()?; let st2 = self.parse_afacs(st1);
+                st2
             },
             Token::Num(_) => {
-                self.parse_atom(); self.parse_afacs();
-            },
+                let st1 = self.parse_atom()?; let st2 = self.parse_afacs(st1);
+                st2
+             },
             Token::True => {
-                self.parse_atom(); self.parse_afacs();
-            },
+                let st1 = self.parse_atom()?; let st2 = self.parse_afacs(st1);
+                st2
+             },
             Token::False => {
-                self.parse_atom(); self.parse_afacs();
-            },
+                let st1 = self.parse_atom()?; let st2 = self.parse_afacs(st1);
+                st2
+             },
             Token::LeftParenthesis => {
-                self.parse_atom(); self.parse_afacs();
-            },
-            _ => panic!(),
+                let st1 = self.parse_atom()?; let st2 = self.parse_afacs(st1);
+                st2
+             },
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
 
-    fn parse_afacs(&mut self) {
+    fn parse_afacs(&mut self, sub: Exp/*impl Ast*/) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
-            Token::Dollar => {},
-            Token::Then => {},
-            Token::Else => {},
-            Token::Do => {},
-            Token::RightCurly => {},
-            Token::Semicolon => {},
-            Token::Or => {},
-            Token::And => {},
-            Token::LessThan =>{},
-            Token::Equals =>{},
-            Token::Plus => {},
-            Token::Minus => {},
+            Token::Dollar => {/**/Ok(sub)},
+            Token::Then => {/**/Ok(sub)},
+            Token::Else => {/**/Ok(sub)},
+            Token::Do => {/**/Ok(sub)},
+            Token::RightCurly => {/**/Ok(sub)},
+            Token::Semicolon => {/**/Ok(sub)},
+            Token::Or => {/**/Ok(sub)},
+            Token::And => {/**/Ok(sub)},
+            Token::LessThan =>{/**/Ok(sub)},
+            Token::Equals =>{/**/Ok(sub)},
+            Token::Plus => {/**/Ok(sub)},
+            Token::Minus => {/**/Ok(sub)},
             Token::Asterisk => {
-                self.eat(Token::Asterisk); self.parse_atom(); self.parse_afacs();
+                self.eat(Token::Asterisk); let s1 = self.parse_atom()?; let s2 = self.parse_afacs(s1)?;
+                Ok(Exp::Times(Box::new(sub), Box::new(s2)))
             },
-            Token::RightParenthesis => {},
-            _ => panic!(),
+            Token::RightParenthesis => {/**/Ok(sub)},
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
     }
-    fn parse_atom(&mut self) {
+    fn parse_atom(&mut self) -> Result<Exp, WhileError> {
         let c = self.peek().unwrap();
         match c {
             Token::Id(s) => {
-                self.eat(Token::Id(s));
+                self.eat(Token::Id(s.clone()));
+                Ok(Exp::Var(s.to_string()))
             },
             Token::Num(s) => {
-                self.eat(Token::Num(s));
+                self.eat(Token::Num(s.clone()));
+                Ok(Exp::Num(s.parse().unwrap()))
             },
             Token::True => {
                 self.eat(Token::True);
+                Ok(Exp::True)
             },
             Token::False => {
                 self.eat(Token::False);
+                Ok(Exp::False)
             },
             Token::LeftParenthesis => {
-                self.eat(Token::LeftParenthesis); self.parse_bexp(); self.eat(Token::RightParenthesis);
+                self.eat(Token::LeftParenthesis); let st = self.parse_bexp()?; self.eat(Token::RightParenthesis);
+                Ok(st)
             },
-            _ => panic!(),
+            _ => Err(WhileError::ParseError),
 
-        };
+        }
 }
 }
 
