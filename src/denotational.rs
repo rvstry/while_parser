73a32f0614c5d5e42403bec31bad3978c873eec5
Ravi Mistry <lh23197@bristol.ac.uk>
2024-12-19 22:20:05 +0000

    add operational semantics

--- src/denotational.rs
+++ src/denotational.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use crate::ast::Exp;
+use crate::ast::{Exp, Stmt};
 
 #[derive(Eq,Hash, PartialEq)]
 enum Variable {
@@ -53,4 +53,27 @@ fn evaluate_boolean(e: &Exp, s: &State) -> bool {
         Exp::Or(e1, e2) => evaluate_boolean(e1, s) || evaluate_boolean(e2, s),
         _ => panic!()
     }
+}
+
+fn execute_statement(statement: &Stmt, state: &mut State) {
+    match statement {
+        Stmt::Skip => (),
+        Stmt::Assn(x, e) => state.update_var(x.clone(), evaluate_arithmetic(e, state)),
+        Stmt::Seq(s1, s2) => {execute_statement(s1, state); execute_statement(s2, state);},
+        Stmt::Cond(e, s1, s2) => {
+            match evaluate_boolean(e, state) {
+                true => execute_statement(s1, state),
+                false => execute_statement(s2, state),
+            };
+        }
+        Stmt::While(e, s) => {
+                match evaluate_boolean(e, state) {
+                    false => (),
+                    true => {
+                        execute_statement(s, state);
+                        execute_statement(statement, state);
+                }
+            }
+        }
+    };
 }
