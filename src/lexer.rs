d890cbb3d0a9995ce88d027f323deaa1a03a9f11
Ravi Mistry <lh23197@bristol.ac.uk>
2024-12-20 10:50:18 +0000

    remove unnecessary references

--- src/lexer.rs
+++ src/lexer.rs
@@ -21,10 +21,10 @@ fn is_more(input: &str) -> bool {
 fn lex_number(input: &str) -> Result<(Token, &str), LexError> {
     let mut lexeme = String::new();
     let mut i = input;
-    while is_more(&i) {
-        let c = peek(&i).ok_or(LexError::WhileError)?;
+    while is_more(i) {
+        let c = peek(i).ok_or(LexError::WhileError)?;
         if c.is_ascii_digit() {
-            i = eat(&i, c)?;
+            i = eat(i, c)?;
             lexeme.push(c);
         }
         else {break};
@@ -36,10 +36,10 @@ fn lex_number(input: &str) -> Result<(Token, &str), LexError> {
 fn lex_kw_or_id(input: &str) -> Result<(Token, &str), LexError> {
     let mut lexeme = String::new();
     let mut i = input;
-    while is_more(&i) {
-        let c = peek(&i).ok_or(LexError::WhileError)?;
+    while is_more(i) {
+        let c = peek(i).ok_or(LexError::WhileError)?;
         if c.is_ascii_alphanumeric() || c == '\'' {
-            i = eat(&i, c)?;
+            i = eat(i, c)?;
             lexeme.push(c);
         }
         else {break};
@@ -65,43 +65,43 @@ pub fn lex(s: &str) -> Result<VecDeque<Token>, LexError> {
     let mut tokens: VecDeque<Token> = VecDeque::new();
 
     let mut i = s;
-    while is_more(&i) {
-        let c = peek(&i).ok_or(LexError::WhileError)?;
+    while is_more(i) {
+        let c = peek(i).ok_or(LexError::WhileError)?;
 
         match c as char {
-            '=' => {i = eat(&i, '=')?; tokens.push_back(Token::Equals)},
-            '!' => {i = eat(&i, '!')?; tokens.push_back(Token::Not)},
-            '+' => {i = eat(&i, '+')?; tokens.push_back(Token::Plus)},
-            '-' => {i = eat(&i, '-')?; tokens.push_back(Token::Minus)},
-            '&' => {i = eat(&i, '&')?; i = eat(&i, '&')?; tokens.push_back(Token::And)}
-            '|' => {i = eat(&i, '|')?; i = eat(&i, '|')?; tokens.push_back(Token::Or)}
+            '=' => {i = eat(i, '=')?; tokens.push_back(Token::Equals)},
+            '!' => {i = eat(i, '!')?; tokens.push_back(Token::Not)},
+            '+' => {i = eat(i, '+')?; tokens.push_back(Token::Plus)},
+            '-' => {i = eat(i, '-')?; tokens.push_back(Token::Minus)},
+            '&' => {i = eat(i, '&')?; i = eat(i, '&')?; tokens.push_back(Token::And)}
+            '|' => {i = eat(i, '|')?; i = eat(i, '|')?; tokens.push_back(Token::Or)}
             '<' => {
-                i = eat(&i, '<')?;
-                if peek(&i).ok_or(LexError::WhileError)? as char == '-' {
-                    i = eat(&i, '-')?;
+                i = eat(i, '<')?;
+                if peek(i).ok_or(LexError::WhileError)? as char == '-' {
+                    i = eat(i, '-')?;
                     tokens.push_back(Token::Assignment)
                 }
                 else {tokens.push_back(Token::LessThan)}
             }
-            '*' => {i = eat(&i, '*')?; tokens.push_back(Token::Asterisk)},
-            '(' => {i = eat(&i, '(')?; tokens.push_back(Token::LeftParenthesis)},
-            ')' => {i = eat(&i, ')')?; tokens.push_back(Token::RightParenthesis)},
-            '{' => {i = eat(&i, '{')?; tokens.push_back(Token::LeftCurly)},
-            '}' => {i = eat(&i, '}')?; tokens.push_back(Token::RightCurly)},
-            ';' => {i = eat(&i, ';')?; tokens.push_back(Token::Semicolon)},
+            '*' => {i = eat(i, '*')?; tokens.push_back(Token::Asterisk)},
+            '(' => {i = eat(i, '(')?; tokens.push_back(Token::LeftParenthesis)},
+            ')' => {i = eat(i, ')')?; tokens.push_back(Token::RightParenthesis)},
+            '{' => {i = eat(i, '{')?; tokens.push_back(Token::LeftCurly)},
+            '}' => {i = eat(i, '}')?; tokens.push_back(Token::RightCurly)},
+            ';' => {i = eat(i, ';')?; tokens.push_back(Token::Semicolon)},
             _ => {
                 if c.is_ascii_digit() {
-                    let (t, out) = lex_number(&i)?;
+                    let (t, out) = lex_number(i)?;
                     i = out;
                     tokens.push_back(t);
                 }
                 if c.is_ascii_lowercase() {
-                    let (t, out) = lex_kw_or_id(&i)?;
+                    let (t, out) = lex_kw_or_id(i)?;
                     i = out;
                     tokens.push_back(t);
                 }
                 if c.is_ascii_whitespace() {
-                    i = eat(&i, c)?;
+                    i = eat(i, c)?;
                 }
             }
 
