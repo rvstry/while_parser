b8c50c18d794a579b9cbb6ff4edd74d5acabc3ad
Ravi Mistry <lh23197@bristol.ac.uk>
2024-12-20 10:08:09 +0000

    move unit tests to parser and lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -114,3 +114,59 @@ pub fn lex(s: &str) -> Result<VecDeque<Token>, LexError> {
 
     Ok(tokens)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use Token::*;
+
+    #[test]
+    fn test0() {
+        assert_eq!(lex("myVar <- x * (foo + bar)"), Ok(VecDeque::from([Id(String::from("myVar")), Assignment, Id(String::from("x")), Asterisk, LeftParenthesis, Id(String::from("foo")), Plus, Id(String::from("bar")), RightParenthesis, Dollar])))
+    }
+
+    #[test]
+    fn test1() {
+        assert_eq!(lex("while true do skip"), Ok(VecDeque::from([While, True, Do, Skip, Dollar])))
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(lex("if x <= 3 then x <- x - 1 else y <- y + 1"), Ok(VecDeque::from([If, Id(String::from("x")), LessThan, Equals, Num(String::from("3")), Then, Id(String::from("x")), Assignment, Id(String::from("x")), Minus, Num(String::from("1")), Else, Id(String::from("y")), Assignment, Id(String::from("y")), Plus, Num(String::from("1")), Dollar])))
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(lex("while y + 3 < 2 do y <- y + 1; x <- 0"), Ok(VecDeque::from([While, Id(String::from("y")), Plus, Num(String::from("3")), LessThan, Num(String::from("2")), Do, Id(String::from("y")), Assignment, Id(String::from("y")), Plus, Num(String::from("1")), Semicolon, Id(String::from("x")), Assignment, Num(String::from("0")), Dollar])))
+    }
+
+    #[test]
+    fn test4() {
+        assert_eq!(lex("y <- y + 1; x <- 0;"), Ok(VecDeque::from([Id(String::from("y")), Assignment, Id(String::from("y")), Plus, Num(String::from("1")), Semicolon, Id(String::from("x")), Assignment, Num(String::from("0")), Semicolon, Dollar])))
+    }
+
+    #[test]
+    fn test5() {
+        assert_eq!(lex("whiley <- (iff + sskip) * doo - thenn"), Ok(VecDeque::from([Id(String::from("whiley")), Assignment, LeftParenthesis, Id(String::from("iff")), Plus, Id(String::from("sskip")), RightParenthesis, Asterisk, Id(String::from("doo")), Minus, Id(String::from("thenn")), Dollar])))
+    }
+
+    #[test]
+    fn test6() {
+        assert_eq!(lex("while a && b && c do skip"), Ok(VecDeque::from([While, Id(String::from("a")), And, Id(String::from("b")), And, Id(String::from("c")), Do, Skip, Dollar])))
+    }
+
+    #[test]
+    fn test7() {
+        assert_eq!(lex("while a || b || c do skip"), Ok(VecDeque::from([While, Id(String::from("a")), Or, Id(String::from("b")), Or, Id(String::from("c")), Do, Skip, Dollar])))
+    }
+
+    #[test]
+    fn test8() {
+        assert_eq!(lex("x <- a + b + c"), Ok(VecDeque::from([Id(String::from("x")), Assignment, Id(String::from("a")), Plus, Id(String::from("b")), Plus, Id(String::from("c")), Dollar])))
+    }
+
+    #[test]
+    fn test9() {
+        assert_eq!(lex("x <- a - b - c"), Ok(VecDeque::from([Id(String::from("x")), Assignment, Id(String::from("a")), Minus, Id(String::from("b")), Minus, Id(String::from("c")), Dollar])))
+    }
+}
